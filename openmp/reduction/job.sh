e9525d66ee8d8de4f5ba4b677c3b5b0b95b45e49
Maxim Yudayev <maxim.yudayev@gmail.com>
2025-06-28 10:05:28 +0300

    Add OMP `reduction` C solution

---
+++ openmp/reduction/job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=reduction
+#SBATCH --account=project_462000956
+#SBATCH --reservation=SummerSchoolCPU
+#SBATCH --output=%x.%J.out
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=00:05:00
+
+srun ./skeleton.x
